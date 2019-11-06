89bc6236a2d4826a42b4978a32e112f8585050a5
tijsyo <55541881+tijsyo@users.noreply.github.com>
2019-11-06 12:34:47 +0100

    Add files via upload

--- taart.js/taarten.js
+++ taart.js/taarten.js
@@ -9,9 +9,9 @@ window.addEventListener('DOMContentLoaded', (event) => { // pas als alle content
 
     for (var i = 0; i < taarten.length; i++) { // loop zorgt ervoor dat alle 4 de elementen worden geselecteerd hij loopt er doorheen //
         taarten[i].addEventListener('click', function () { // hij zoekt eerst op welke je klikt van de containers en verolgens zet hij de opacity naar 0 //
-            this.style.opacity = "0"; // this is het huidige element die in de loop an de beurt is // style is een element aanpassen naar opacity 0 //
+
+            this.classList.add('taartjs'); // this is het huidige element die in de loop an de beurt is // style is een element aanpassen naar opacity 0 //
         });
     }
 });
 
-// source = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
